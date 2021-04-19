cda00a485013458fec237821a4ff2a92bf3bda51
erick <erickgordo64@gmail.com>
2021-04-19 02:27:15 -0600

    consultas bd

--- mia-proyecto-final/src/App.js
+++ mia-proyecto-final/src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { BrowserRouter, Route, Link, Router } from "react-router-dom";
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Navegar from './components/navigate';
 import Estudiante from './components/estudiante';
 
@@ -9,7 +9,7 @@ function App() {
   return (
     <Router>
       <Navegar/>
-      <Route path="/estudiante" component={Estudiante}/>
+      <Route path="/estudiante" component={Estudiante}></Route>
     </Router>
   );
 }
--- mia-proyecto-final/src/components/estudiante.js
+++ mia-proyecto-final/src/components/estudiante.js
@@ -3,7 +3,37 @@ import { Link } from 'react-router-dom'
 import axios from 'axios'
 
 export default class Estudiante extends Component{
+    
+    state ={
+        alumnos:[]
+    }
+
+    async componentDidMount() {
+        this.getActividades();
+    }
+
+    getActividades = async () => {
+        const res = await axios.get('http://localhost:4000/data');
+        this.setState({
+            alumnos: res.data
+        });
+        console.log(res.data);
+    }
+    
+
     render(){
-        return(<h1>hola</h1>)
+        return(
+
+        <div>
+            <h1>lista de alumnos</h1>
+            <ul className="list-group">
+                {
+                    this.state.alumnos.map(alumno=>(
+                        <li className="list-group-item" key={alumno.Nombre}>{alumno.Nombre+" "+alumno.Apellido}</li>
+                    ))
+                }
+            </ul>
+        </div>
+        )
     }
 }
