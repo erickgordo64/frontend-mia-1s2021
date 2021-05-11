d9f895fce0427623e283771c8b9475bdf00b1edd
erick <erickgordo64@gmail.com>
2021-05-11 03:49:12 -0600

    reporetes

--- mia-proyecto-final/src/components/reportes.js
+++ mia-proyecto-final/src/components/reportes.js
@@ -2,19 +2,18 @@ import React, { Component, useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import Cookies from 'universal-cookie';
-import { Bar, Pie, Doughnut } from 'react-chartjs-2';
+import { Bar, Pie, Doughnut, Line } from 'react-chartjs-2';
 import Navigate from './navigate';
 
-const state = {
-    labels: ['January', 'February', 'March',
-        'April', 'May'],
+const  state = {
+    labels: [],
     datasets: [
         {
             label: 'Rainfall',
             backgroundColor: 'rgba(75,192,192,1)',
             borderColor: 'rgba(0,0,0,1)',
             borderWidth: 2,
-            data: [65, 59, 80, 81, 56]
+            data: []
         }
     ]
 }
@@ -44,7 +43,48 @@ const state2 = {
     ]
 }
 
+const data = {
+    labels: [],
+    datasets: [
+        {
+            label: 'My First dataset',
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: 'rgba(75,192,192,0.4)',
+            borderColor: 'rgba(75,192,192,1)',
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: 'rgba(75,192,192,1)',
+            pointBackgroundColor: '#fff',
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+            pointHoverBorderColor: 'rgba(220,220,220,1)',
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: [65, 59, 80, 81, 56, 55, 40]
+        }
+    ]
+};
+
+
 export default class Reportes extends Component {
+
+   
+
+    componentDidMount() {
+        this.getActividades();
+    }
+
+    getActividades = async () => {
+        const res = await axios.get('http://localhost:4000/tasks');
+        state.labels=res.data.map(id=>(id.ID))
+        console.log(res.data.map(id=>(id.ID)));
+    }
+
     render() {
         return (
             <div>
@@ -94,6 +134,26 @@ export default class Reportes extends Component {
                         <div class="col align-self-center">
                             <Doughnut
                                 data={state2}
+                                options={{
+                                    title: {
+                                        display: true,
+                                        text: 'Average Rainfall per month',
+                                        fontSize: 20
+                                    },
+                                    legend: {
+                                        display: true,
+                                        position: 'right'
+                                    }
+                                }}
+                            />
+                        </div>
+                    </div>
+                </div>
+                <div class="container">
+                    <div class="row">
+                        <div class="col align-self-center">
+                            <Line
+                                data={data}
                                 options={{
                                     title: {
                                         display: true,
