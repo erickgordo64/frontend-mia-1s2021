89eda136371570d8c4a5e39c4769f347cbfaa1fd
erick <erickgordo64@gmail.com>
2021-05-11 21:02:48 -0600

    cards de ventos

--- mia-proyecto-final/src/components/temporada.js
+++ mia-proyecto-final/src/components/temporada.js
@@ -43,57 +43,69 @@ export default class Temporada extends Component {
         return (
             <div>
                 <Navigate />
-                <div class="container">
-                    <div class="row align-items-start">
-                        <form onSubmit={this.onSubmit}>
-                            <h5>Nombre Temporada</h5>
-                            <input
-                                id="nombret"
-                                type="text"
-                                className="form-control"
-                            />
-                            <h5>fecha inicio</h5>
-                            <input
-                                id="fechai"
-                                type="date"
-                                className="form-control"
-                            />
-                            <h5>fecha fin</h5>
-                            <input
-                                id="fechaf"
-                                type="date"
-                                className="form-control"
-                            />
-                            <br></br>
-                            <br></br>
-                            <button type="submit" className="btn btn-danger" onClick={(e) => this.onSubmitcolaborador(e)}>Registrar</button>
-                        </form>
+                <br></br>
+                <div class="row">
+                    <div class="col-sm-6">
+                        <div class="card">
+                        <h5 class="card-header">Agregar una temporada</h5>
+                            <div class="card-body">
+                                <form onSubmit={this.onSubmit}>
+                                    <h5>Nombre Temporada</h5>
+                                    <input
+                                        id="nombret"
+                                        type="text"
+                                        className="form-control"
+                                    />
+                                    <h5>fecha inicio</h5>
+                                    <input
+                                        id="fechai"
+                                        type="date"
+                                        className="form-control"
+                                    />
+                                    <h5>fecha fin</h5>
+                                    <input
+                                        id="fechaf"
+                                        type="date"
+                                        className="form-control"
+                                    />
+                                    <br></br>
+                                    <br></br>
+                                    <button type="submit" className="btn btn-danger" onClick={(e) => this.onSubmitcolaborador(e)}>Registrar</button>
+                                </form>
+
+                            </div>
+                        </div>
                     </div>
-                    <div class="row align-items-end">
-                        <table className="table">
-                            <thead>
-                                <tr>
-                                    <th scope="col">#</th>
-                                    <th scope="col">nombre</th>
-                                    <th scope="col">fecha inicio</th>
-                                    <th scope="col">fecha fin</th>
-                                    <th scope="col">estado</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {
-                                    this.state.alumnos.map(post => {
-                                        return <tr key={post.idtemporada}>
-                                            <th scope="row">{post.idtemporada}</th>
-                                            <td>{post.Nombre}</td>
-                                            <td>{post.fecha_inicio}</td>
-                                            <td>{post.fecha_fin}</td>
-                                            <td>{post.Estado}</td>
+                    <div class="col-sm-6">
+                        <div class="card">
+                        <h5 class="card-header">Temporadas registradas</h5>
+                            <div class="card-body">
+                                <table className="table">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">#</th>
+                                            <th scope="col">nombre</th>
+                                            <th scope="col">fecha inicio</th>
+                                            <th scope="col">fecha fin</th>
+                                            <th scope="col">estado</th>
                                         </tr>
-                                    })
-                                }
-                            </tbody>
-                        </table>
+                                    </thead>
+                                    <tbody>
+                                        {
+                                            this.state.alumnos.map(post => {
+                                                return <tr key={post.idtemporada}>
+                                                    <th scope="row">{post.idtemporada}</th>
+                                                    <td>{post.Nombre}</td>
+                                                    <td>{post.fecha_inicio}</td>
+                                                    <td>{post.fecha_fin}</td>
+                                                    <td>{post.Estado}</td>
+                                                </tr>
+                                            })
+                                        }
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
